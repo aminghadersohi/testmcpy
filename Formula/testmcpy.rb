43d875717224a71a1bd0f4d920358ec0a8aa92f3
Amin Ghadersohi <amin.ghadersohi@gmail.com>
2026-06-12 16:02:50 -0400

    Update Homebrew formula for v0.10.3

--- Formula/testmcpy.rb
+++ Formula/testmcpy.rb
@@ -3,8 +3,8 @@ class Testmcpy < Formula
 
   desc "MCP Testing Framework - Test LLM tool calling with MCP services"
   homepage "https://github.com/preset-io/testmcpy"
-  url "https://files.pythonhosted.org/packages/source/t/testmcpy/testmcpy-0.8.0.tar.gz"
-  sha256 "132253c433db95758cabdaf349516eb0d9dd3811207fe722fdff4eda9cca6851"
+  url "https://files.pythonhosted.org/packages/source/t/testmcpy/testmcpy-0.10.3.tar.gz"
+  sha256 "170dc2f6694e03385f3fd2da409329ed67fad00875af9335a8eca24ce533ecc6"
   license "Apache-2.0"
 
   depends_on "python@3.11"
